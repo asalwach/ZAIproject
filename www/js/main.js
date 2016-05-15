8ed0af2f8d0f05fc10f4b3d3b6a9307ccd773b81
Adrian Salwach <asalwach@gmail.com>
2016-05-15 15:48:30 +0200

    current indexes added

--- www/js/main.js
+++ www/js/main.js
@@ -9,9 +9,41 @@ var main = {
     },
 
     current: function () {
-        $(document).on("deviceready", function () {
+    //    $(document).on("deviceready", function () {
+	        $(document).on("pageshow", function (e) {
+				if ($(e.target).attr('id') == 's2') {
 
-        });
+					var indexes = ["TWTR", "FB", "GOOGL", "NFLX", "AAPL", "GOOG", "MSFT", "INTC", "CSCO", "QCOM", "TXN", "AVGO", "ADBE", "BIDU"];
+					var iLen = indexes.length;
+					var idArray = "";
+						for (var j = 0; j < iLen; j++) {
+							idArray += "'" + indexes[j] + "'";
+							if (j != (iLen-1))
+								idArray += ", ";
+						}
+					
+					var url = "http://query.yahooapis.com/v1/public/yql";
+					$("#tableBody").empty();
+					var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in (" + idArray + ")");
+					$.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
+					.done(function (data) {
+						for (let i = 0; i < iLen; i++) {							
+							$("#tableBody").append("<tr>" + 
+											"<td>" + (i+1) + "</td>" +
+											"<td>" + data.query.results.quote[i].symbol + "</td>" +
+											"<td>" + data.query.results.quote[i].LastTradePriceOnly + "</td>" +
+											"<td>" + data.query.results.quote[i].Change + "</td>" +
+											"<td>" + data.query.results.quote[i].ChangeinPercent + "</td>" +
+										"</tr>");
+						}				
+					})
+					.fail(function (jqxhr, textStatus, error) {
+					var err = textStatus + ", " + error;
+						$("#tableBody").text('Sprawdzenie nieudane: ' + err);
+					});	
+				}
+		    });
+    //    });
     },
 	
     history: function () {
@@ -25,11 +57,11 @@ var main = {
 
 		var url = "http://query.yahooapis.com/v1/public/yql";
 		var symbol = $("#symbol").val();
-		var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
+		var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('TWTR', 'FB', 'GOOGL', 'NFLX')");
 
 		$.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
 			.done(function (data) {
-			$("#jsonik").text("json: " + JSON.stringify(data));
+			$("#jsonik").text("json: " + JSON.stringify(data.query.results.quote[2]));
 			$("#result").text("" + data.query.results.quote.LastTradePriceOnly);
 		})
 			.fail(function (jqxhr, textStatus, error) {
@@ -53,7 +85,7 @@ var main = {
 
 $(function() {
   $("#header").hide();
-  setTimeout(hideSplash, 4000);
+  setTimeout(hideSplash, 2000);
 });
 
 function hideSplash() {
